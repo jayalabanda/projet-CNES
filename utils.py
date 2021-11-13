2fd19e1b1bc5554ff4fcca8ccad855cc697a180c
jayalabanda <49167913+jayalabanda@users.noreply.github.com>
2021-11-13 15:04:06 +0100

    new functions and testing

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import os
 import cv2
-from datetime import date
 import json
 import itertools
 
@@ -14,12 +13,42 @@ from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 import utm
 
 
+def select_image_cloud(images_df, cloud_threshold):
+    """Select images with cloud cover less than cloud_threshold.
+       Return the one with the lowest cloud coverage. 
+
+    Arguments:
+        images_df (dataframe) -- dataframe containing images
+        cloud_threshold (float) -- threshold for cloud coverage
+
+    Returns:
+        uuid (string) -- uuid of the selected image.
+    """
+    images_df = images_df[images_df.cloudcoverpercentage < cloud_threshold]
+    best_image = images_df[images_df.cloudcoverpercentage ==
+                           images_df.cloudcoverpercentage.min()]
+
+    return best_image.iloc[0]["uuid"]
+
+
+def download_from_api(df, api, index):
+    """Downloads images from the API and saves them to the disk as a zip file.
+
+    Args:
+        df (dataframe): dataframe containing the images to download
+        api (SentinelAPI): API object
+        index (int): index of the image to download
+    """
+    uuid = df["uuid"].values[index]
+    api.download(uuid)
+
+
 def get_band(image_folder, band, resolution=10):
     """Returns an image opened with rasterio with the given band and resolution    
 
     Args:
-        image_folder (path): path to the folder containing the image
-        band (string): name of the band to be extracted
+        image_folder (str): path to the folder containing the image
+        band (str): name of the band to be extracted
         resolution (int): Resolution of the image. Defaults to 10.
 
     Returns:
@@ -42,7 +71,7 @@ def threshold_filter(image, threshold):
     """Puts all values below threshold to 0.
 
     Args:
-        image : Already imported image
+        image: Already imported image
         threshold (float): Threshold value
 
     Returns:
@@ -53,16 +82,15 @@ def threshold_filter(image, threshold):
 
 
 def calculate_area(image):
-    """Calculates the area of the image.
+    """Calculates the area of the burnt area.
 
     Args:
-        image : Already imported image
+        image: Already imported image
 
     Returns:
         area: area of the image
     """
     count = np.count_nonzero(image)
-    # ndarray.size = n * m
     ratio = count / image.size
     # multiply ratio by the actual area of the image using coordinates
     # remove the line below once the area using coordinates is calculated has been implemented.
@@ -74,10 +102,12 @@ def merge_four_images(image_array):
     """
     Takes 4 images of SAME SIZE, merges them together to get a lager field of view
     along with a bigger final picture.
+
     Args:
         image_array (list): list of the 4 images that need to be merged.
         First image: upper left, second image: upper right.
-        Third image: lower left, fourth image: lower right. 
+        Third image: lower left, fourth image: lower right.
+
     Returns:
         final_mage: one final image that has all 4 images merged together
     """,
